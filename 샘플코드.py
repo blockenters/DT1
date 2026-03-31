e618f36e809ce7c513f5320d41884cde32e663ce
blockenters <58394003+blockenters@users.noreply.github.com>
2026-03-31 20:20:34 +0900

    Create 샘플코드.py

---
+++ 샘플코드.py
@@ -0,0 +1,7 @@
+employees = [
+    {"name": "김철수", "dept": "영업", "salary": 3500000},
+    {"name": "이영희", "dept": "마케팅", "salary": 4200000},
+    {"name": "박민수", "dept": "영업", "salary": 3800000},
+    {"name": "최지은", "dept": "개발", "salary": 5100000},
+    {"name": "정하나", "dept": "마케팅", "salary": 3900000},
+]
